2eeaff7d158ee9347256d1255a01b9e8de487bc4
javokhirm <javokhirmdev@gmail.com>
2022-12-25 22:07:17 +0500

    year

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <span>2</span>
+      <span>0</span>
+      <span>2</span>
+      <div className="remove">
+        <div className="ball2">
+          <div className="ballon"></div>
+          <div className="stick"></div>
+        </div>
+        <span className="newNum">2</span>
+      </div>
+
+      <div className="new">
+        <div className="ball">
+          <div className="ballon"></div>
+          <div className="stick"></div>
+        </div>
+        <span className="newNum">3</span>
+      </div>
+    </div>
+  );
+}
+
+export default App;
